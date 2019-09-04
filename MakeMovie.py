9d2cf884ed6fcba439c49b5d63c6c7c2cd1d83b7
James Wellnitz <wellnitz.james@gmail.com>
2019-09-03 22:58:07 -0400

    Intial commit

---
+++ MakeMovie.py
@@ -0,0 +1,7 @@
+"""
+@author James Wellnitz
+@email wellnitz.james@gmail.com
+This program is designed help set up the usage oof python scripts for PyMOL
+It is meant as a learning device and not used for research purposes
+"""
+
